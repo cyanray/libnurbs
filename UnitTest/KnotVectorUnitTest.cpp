83d56cdd5ec0695c9da8c9b39609e2f636a6dcc7
Cyan <zyanglin@outlook.com>
2024-01-02 23:17:27 +0800

    Update: KnotVectorUnitTest.cpp

--- UnitTest/KnotVectorUnitTest.cpp
+++ UnitTest/KnotVectorUnitTest.cpp
@@ -9,20 +9,46 @@ using namespace Catch;
 using namespace libnurbs;
 using namespace std;
 
-TEST_CASE("Core/KnotVector", "[libnurbs_knotvector]")
+
+TEST_CASE("KnotVector/Constructor", "[knot_vector, constructor]")
 {
-    SECTION("Constructor")
+    SECTION("simple")
     {
         REQUIRE_NOTHROW(KnotVector{{0.0, 0.0, 0.0, 1.0, 1.0, 1.0}});
         REQUIRE_NOTHROW(KnotVector{{0.0, 0.0, 0.0, 0.5, 1.0, 1.0, 1.0}});
         REQUIRE_NOTHROW(KnotVector{{0.0, 1.0}});
         REQUIRE_NOTHROW(KnotVector{{0.0, 0.0, 1.0, 1.0}});
 
-        REQUIRE_THROWS_AS(  (KnotVector{{0.0, 0.0, 1.0, 2.0}})  , invalid_argument);
-        REQUIRE_THROWS_AS(  (KnotVector{{0.0, 0.0, 2.0, 1.0}})  , invalid_argument);
+        REQUIRE_THROWS_AS((KnotVector{{0.0, 0.0, 1.0, 2.0}}), invalid_argument);
+        REQUIRE_THROWS_AS((KnotVector{{0.0, 0.0, 2.0, 1.0}}), invalid_argument);
     }
 
-    SECTION("KnotPairs")
+    SECTION("static KnotVector::Uniform()")
+    {
+        KnotVector knots = KnotVector::Uniform(3, 10);
+        REQUIRE(knots.Count() == 10);
+
+        auto result = knots.GetKnotPairs();
+        REQUIRE(result.size() == 4);
+        REQUIRE(result[0].Value == 0.0);
+        REQUIRE(result[0].Multiplicity == 4);
+
+        REQUIRE(result[1].Value == Approx(0.33333333333));
+        REQUIRE(result[1].Multiplicity == 1);
+
+        REQUIRE(result[2].Value == Approx(0.66666666666));
+        REQUIRE(result[2].Multiplicity == 1);
+
+        REQUIRE(result[3].Value == 1.0);
+        REQUIRE(result[3].Multiplicity == 4);
+    }
+
+}
+
+
+TEST_CASE("KnotVector/GetKnotPairs", "[knot_vector, get_knot_pairs]")
+{
+    SECTION("simple")
     {
         KnotVector U{{0.0, 0.0, 0.0, 0.5, 1.0, 1.0, 1.0}};
         auto result = U.GetKnotPairs();
@@ -34,8 +60,12 @@ TEST_CASE("Core/KnotVector", "[libnurbs_knotvector]")
         REQUIRE(result[2].Value == 1.0);
         REQUIRE(result[2].Multiplicity == 3);
     }
+}
 
-    SECTION("FindSpanIndex")
+
+TEST_CASE("KnotVector/FindSpanIndex", "[knot_vector, find_span_index]")
+{
+    SECTION("simple")
     {
         KnotVector U{{0.0, 0.0, 0.0, 0.3, 0.5, 0.75, 1.0, 1.0, 1.0}};
         REQUIRE(U.FindSpanIndex(2, 0.0) == 2);
@@ -49,7 +79,23 @@ TEST_CASE("Core/KnotVector", "[libnurbs_knotvector]")
         REQUIRE(U.FindSpanIndex(2, 0.8) == 5);
         REQUIRE(U.FindSpanIndex(2, 0.9) == 5);
         REQUIRE(U.FindSpanIndex(2, 1.0) == 5);
-        REQUIRE(U.FindSpanIndex(2, 1.1) == 5);  // u=1.1 is an illegal input, but acceptable
+        REQUIRE(U.FindSpanIndex(2, 1.1) == 5); // u=1.1 is an illegal input, but acceptable
     }
 
+    SECTION("simple2")
+    {
+        KnotVector U{{0.0, 0.0, 0.0, 0.3, 0.5, 0.5, 0.75, 1.0, 1.0, 1.0}};
+        REQUIRE(U.FindSpanIndex(2, 0.0) == 2);
+        REQUIRE(U.FindSpanIndex(2, 0.1) == 2);
+        REQUIRE(U.FindSpanIndex(2, 0.2) == 2);
+        REQUIRE(U.FindSpanIndex(2, 0.3) == 3);
+        REQUIRE(U.FindSpanIndex(2, 0.4) == 3);
+        REQUIRE(U.FindSpanIndex(2, 0.5) == 5);
+        REQUIRE(U.FindSpanIndex(2, 0.6) == 5);
+        REQUIRE(U.FindSpanIndex(2, 0.7) == 5);
+        REQUIRE(U.FindSpanIndex(2, 0.8) == 6);
+        REQUIRE(U.FindSpanIndex(2, 0.9) == 6);
+        REQUIRE(U.FindSpanIndex(2, 1.0) == 6);
+        REQUIRE(U.FindSpanIndex(2, 1.1) == 6); // u=1.1 is an illegal input, but acceptable
+    }
 }
