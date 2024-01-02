638bb654966a6f5a961b504e8683e758698a1961
cyanray <zyanglin@outlook.com>
2024-01-02 12:46:22 +0800

    Update: GeomSegment::GetCurve().

--- src/Geometry/GeomSegment.cpp
+++ src/Geometry/GeomSegment.cpp
@@ -11,26 +11,12 @@ namespace libnurbs
         const int knot_count = ControlPointCount + Degree + 1;
         Curve curve;
         curve.Degree = Degree;
-        auto& knots = curve.Knots.Values();
-        knots.resize(knot_count);
-        for (int i = 0; i < Degree + 1; ++i)
-        {
-            knots[i] = 0.0;
-        }
-        const Numeric t = 1.0 / (knot_count - (Degree + 1) * 2);
-        for (int i = Degree + 1; i < knot_count - (Degree + 1); ++i)
-        {
-            knots[i] = i * t;
-        }
-        for (int i = knot_count - (Degree + 1); i < knot_count; ++i)
-        {
-            knots[i] = 1.0;
-        }
+        curve.Knots = KnotVector::Uniform(Degree, knot_count);
         curve.ControlPoints.resize(ControlPointCount);
         const Vec3 k = (RightPoint - LeftPoint) / (ControlPointCount - 1);
         for (int i = 0; i < ControlPointCount; ++i)
         {
-            curve.ControlPoints[i] = LeftPoint + i * k;
+            curve.ControlPoints[i].head<3>() = LeftPoint + i * k;
         }
         return curve;
     }
