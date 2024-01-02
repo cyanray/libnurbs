c9c0ebf393224d0991171e75a8e38c1b9ed0a0b8
cyanray <zyanglin@outlook.com>
2024-01-02 12:47:44 +0800

    Add: class GeomRect.

--- include/libnurbs/Geometry/GeomRect.hpp
+++ include/libnurbs/Geometry/GeomRect.hpp
@@ -1,6 +1,27 @@
 #pragma once
 
+#include "libnurbs/Core/Typedefs.hpp"
+
 namespace libnurbs
 {
-    
+    class Surface;
+}
+
+namespace libnurbs
+{
+    class GeomRect
+    {
+    public:
+        int DegreeU{1};
+        int DegreeV{1};
+        int ControlPointCountU{2};
+        int ControlPointCountV{2};
+        Vec3 LeftBottomPoint = Vec3::Zero();
+        Vec3 RightBottomPoint = Vec3::Zero();
+        Vec3 LeftTopPoint = Vec3::Zero();
+        Vec3 RightTopPoint = Vec3::Zero();
+
+    public:
+        [[nodiscard]] Surface GetSurface() const;
+    };
 }
---
+++ src/Geometry/GeomRect.cpp
@@ -0,0 +1,32 @@
+#include "libnurbs/Geometry/GeomRect.hpp"
+
+#include "libnurbs/Surface/Surface.hpp"
+
+namespace libnurbs
+{
+    Surface GeomRect::GetSurface() const
+    {
+        assert(DegreeU >= 1);
+        assert(DegreeV >= 1);
+        assert(ControlPointCountU >= DegreeU + 1);
+        assert(ControlPointCountV >= DegreeV + 1);
+        const int knot_count_u = ControlPointCountU + DegreeU + 1;
+        const int knot_count_v = ControlPointCountV + DegreeV + 1;
+        Surface surface;
+        surface.DegreeU = DegreeU;
+        surface.DegreeV = DegreeV;
+        surface.KnotsU = KnotVector::Uniform(DegreeU, knot_count_u);
+        surface.KnotsV = KnotVector::Uniform(DegreeV, knot_count_v);
+        surface.ControlPoints = {ControlPointCountU, ControlPointCountV};
+        Vec3 k1 = (RightBottomPoint - LeftBottomPoint) / (ControlPointCountU - 1);
+        Vec3 k2 = (LeftTopPoint - LeftBottomPoint) / (ControlPointCountV - 1);
+        for (int i = 0; i < ControlPointCountU; ++i)
+        {
+            for (int j = 0; j < ControlPointCountV; ++j)
+            {
+                surface.ControlPoints.get(i, j).head<3>() = LeftBottomPoint + i * k1 + j * k2;
+            }
+        }
+        return surface;
+    }
+}
