88db6bb36292e2b935b05631170b80450fffab7f
cyanray <zyanglin@outlook.com>
2024-01-23 19:30:39 +0800

    Add: Benchmark.

---
+++ Benchmark/BM_Basis.cpp
@@ -0,0 +1,29 @@
+#include <random>
+#include <benchmark/benchmark.h>
+#include <libnurbs/Basis/BSplineBasis.hpp>
+#include <libnurbs/Core/KnotVector.hpp>
+
+using namespace libnurbs;
+
+static void BM_Basis_Evaluate(benchmark::State& state)
+{
+    int degree = 3;
+    KnotVector U{{0.0, 0.0, 0.0, 0.0, 0.5, 1.0, 1.0, 1.0, 1.0}};
+    std::random_device rand_dev;
+    std::mt19937 generator(rand_dev());
+    std::uniform_real_distribution<double> distr(0.0, 1.0);
+    for (auto _: state)
+    {
+        state.PauseTiming();
+        double u = distr(generator);
+        state.ResumeTiming();
+        BSplineBasis::Evaluate(degree, U, u);
+    }
+}
+BENCHMARK(BM_Basis_Evaluate);
+
+
+
+
+// Run the benchmark
+BENCHMARK_MAIN();
