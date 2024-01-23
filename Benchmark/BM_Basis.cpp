7fb583e21b31cbb646e61af7f5bac701b49cbb2c
cyanray <zyanglin@outlook.com>
2024-01-23 19:42:10 +0800

    Add: Benchmark, BM_Basis_EvaluateDerivative.

--- Benchmark/BM_Basis.cpp
+++ Benchmark/BM_Basis.cpp
@@ -22,6 +22,23 @@ static void BM_Basis_Evaluate(benchmark::State& state)
 }
 BENCHMARK(BM_Basis_Evaluate);
 
+static void BM_Basis_EvaluateDerivative(benchmark::State& state)
+{
+    int degree = 3;
+    KnotVector U{{0.0, 0.0, 0.0, 0.0, 0.5, 1.0, 1.0, 1.0, 1.0}};
+    std::random_device rand_dev;
+    std::mt19937 generator(rand_dev());
+    std::uniform_real_distribution<double> distr(0.0, 1.0);
+    for (auto _: state)
+    {
+        state.PauseTiming();
+        double u = distr(generator);
+        state.ResumeTiming();
+        BSplineBasis::EvaluateDerivative(degree, U, u, 2);
+    }
+}
+BENCHMARK(BM_Basis_EvaluateDerivative);
+
 
 
 
