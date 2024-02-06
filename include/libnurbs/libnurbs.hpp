ee3e6209b0708b55fce43b5cdaa3c936974b3e74
cyanray <zyanglin@outlook.com>
2024-02-07 01:54:29 +0800

    Update: libnurbs.hpp;

--- include/libnurbs/libnurbs.hpp
+++ include/libnurbs/libnurbs.hpp
@@ -5,4 +5,25 @@
 #ifndef LIBNURBS_LIBNURBS_HPP
 #define LIBNURBS_LIBNURBS_HPP
 
+/* Algotithm */
+#include "libnurbs/Algorithm/MathUtils.hpp"
+
+/* Basis */
+#include "libnurbs/Basis/BSplineBasis.hpp"
+
+/* Core */
+#include "libnurbs/Core/Typedefs.hpp"
+#include "libnurbs/Core/Grid.hpp"
+#include "libnurbs/Core/KnotVector.hpp"
+
+/* Geometry */
+#include "libnurbs/Geometry/GeomRect.hpp"
+#include "libnurbs/Geometry/GeomSegment.hpp"
+
+/* Curve */
+#include "libnurbs/Curve/Curve.hpp"
+
+/* Surface */
+#include "libnurbs/Surface/Surface.hpp"
+
 #endif //LIBNURBS_LIBNURBS_HPP
