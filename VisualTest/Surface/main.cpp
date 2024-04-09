f74883b6a32f229c4056bbcfa9e39737b27ff26d
cyanray <zyanglin@outlook.com>
2024-04-09 19:47:04 +0800

    Update: VisualTest/Surface

--- VisualTest/Surface/main.cpp
+++ VisualTest/Surface/main.cpp
@@ -1,64 +1,38 @@
-#include <iostream>
-#include <libnurbs/Basis/BSplineBasis.hpp>
-#include <libnurbs/Core/KnotVector.hpp>
-#include <Eigen/Dense>
-
-#include <matplot/matplot.h>
-
 #include <vector>
-
 #include <libnurbs/Core/Grid.hpp>
-
+#include <libnurbs/Core/KnotVector.hpp>
 #include <libnurbs/Surface/Surface.hpp>
+#include <matplot/matplot.h>
 
 using namespace std;
 using namespace libnurbs;
 using namespace matplot;
 
-void CaseA()
+void ShowSurface(const Surface& surface, double step = 0.1)
 {
-    ControlPointGrid grid;
-    grid.UCount = 3;
-    grid.VCount = 2;
-    grid.Values.emplace_back(1, 0, 0, 1);
-    grid.Values.emplace_back(1, 1, 0, 1);
-    grid.Values.emplace_back(0, 1, 0, 2);
-    grid.Values.emplace_back(1, 0, 1, 1);
-    grid.Values.emplace_back(1, 1, 1, 1);
-    grid.Values.emplace_back(0, 1, 1, 2);
+    using namespace matplot;
+    auto [xi, eta] = meshgrid(iota(0, step, 1));
+    size_t len = xi.size();
+    std::vector X(len, std::vector<double>(len));
+    std::vector Y(len, std::vector<double>(len));
+    std::vector Z(len, std::vector<double>(len));
 
-    Surface surface;
-    surface.DegreeU = 2;
-    surface.DegreeV = 1;
-    surface.KnotsU = KnotVector{{0.0, 0.0, 0.0, 1.0, 1.0, 1.0}};
-    surface.KnotsV = KnotVector{{0.0, 0.0, 1.0, 1.0}};
-    surface.ControlPoints = grid;
-
-    VecX s = VecX::LinSpaced(20, 0.0, 1.0);
-    VecX t = VecX::LinSpaced(20, 0.0, 1.0);
-
-    vector<double> px;
-    px.reserve(s.size());
-    vector<double> py;
-    py.reserve(s.size());
-    vector<double> pz;
-    pz.reserve(s.size());
-
-    for (auto x: s)
+    for (size_t i = 0; i < len; ++i)
     {
-        for (auto y: t)
+        for (size_t j = 0; j < len; ++j)
         {
-            auto value = surface.Evaluate(x, y);
-            px.push_back(value.x());
-            py.push_back(value.y());
-            pz.push_back(value.z());
+            auto eval_result = surface.Evaluate(xi[i][j], eta[i][j]);
+            X[i][j] = eval_result.x();
+            Y[i][j] = eval_result.y();
+            Z[i][j] = eval_result.z();
         }
     }
-    plot3(px, py, pz, "r.");
+    surf(X, Y, Z)->face_alpha(0.5).edge_color("none");
     show();
 }
 
-void CaseB()
+
+Surface CaseSemiCylinder()
 {
     ControlPointGrid grid;
     grid.UCount = 3;
@@ -77,43 +51,11 @@ void CaseB()
     surface.KnotsV = KnotVector{{0.0, 0.0, 1.0, 1.0}};
     surface.ControlPoints = grid;
 
-    VecX s = VecX::LinSpaced(20, 0.0, 1.0);
-    VecX t = VecX::LinSpaced(20, 0.0, 1.0);
-
-    vector<double> px;
-    px.reserve(s.size());
-    vector<double> py;
-    py.reserve(s.size());
-    vector<double> pz;
-    pz.reserve(s.size());
-
-    for (const auto x: s)
-    {
-        auto value = surface.Evaluate(x, 0.0);
-        px.push_back(value.x());
-        py.push_back(value.y());
-        pz.push_back(value.z());
-        value = surface.Evaluate(x, 1.0);
-        px.push_back(value.x());
-        py.push_back(value.y());
-        pz.push_back(value.z());
-    }
-    for (const auto y: t)
-    {
-        auto value = surface.Evaluate(0.0, y);
-        px.push_back(value.x());
-        py.push_back(value.y());
-        pz.push_back(value.z());
-        value = surface.Evaluate(1.0, y);
-        px.push_back(value.x());
-        py.push_back(value.y());
-        pz.push_back(value.z());
-    }
-    plot3(px, py, pz, "r.");
-    show();
+    return surface;
 }
 
-void CaseC()
+
+Surface CaseCylinder()
 {
     double w = sqrt(2.0) / 2.0;
 
@@ -147,111 +89,41 @@ void CaseC()
     surface.KnotsV = KnotVector{{0.0, 0.0, 1.0, 1.0}};
     surface.ControlPoints = grid;
 
-    VecX s = VecX::LinSpaced(100, 0.0, 1.0);
-    VecX t = VecX::LinSpaced(8, 0.0, 1.0);
-
-    vector<double> px;
-    px.reserve(s.size());
-    vector<double> py;
-    py.reserve(s.size());
-    vector<double> pz;
-    pz.reserve(s.size());
-
-    for (auto x: s)
-    {
-        for (auto y: t)
-        {
-            auto value = surface.Evaluate(x, y);
-            px.push_back(value.x());
-            py.push_back(value.y());
-            pz.push_back(value.z());
-        }
-    }
-    plot3(px, py, pz, "r.");
-    show();
+    return surface;
 }
 
-void CaseD()
+
+
+Surface CaseSaddle()
 {
-    double w = sqrt(2.0) / 2.0;
-
     ControlPointGrid grid;
-    grid.UCount = 9;
-    grid.VCount = 2;
-    grid.Values.emplace_back(0.5, 0.0, 0.0, 1);
-    grid.Values.emplace_back(1.0, 0.0, 0.0, w);
-    grid.Values.emplace_back(1.0, 0.5, 0.0, 1);
-    grid.Values.emplace_back(1.0, 1.0, 0.0, w);
-    grid.Values.emplace_back(0.5, 1.0, 0.0, 1);
-    grid.Values.emplace_back(0.0, 1.0, 0.0, w);
-    grid.Values.emplace_back(0.0, 0.5, 0.0, 1);
-    grid.Values.emplace_back(0.0, 0.0, 0.0, w);
-    grid.Values.emplace_back(0.5, 0.0, 0.0, 1);
-
-    grid.Values.emplace_back(0.5, 0.0, 3.0, 1);
-    grid.Values.emplace_back(1.0, 0.0, 3.0, w);
-    grid.Values.emplace_back(1.0, 0.5, 3.0, 1);
-    grid.Values.emplace_back(1.0, 1.0, 3.0, w);
-    grid.Values.emplace_back(0.5, 1.0, 3.0, 1);
-    grid.Values.emplace_back(0.0, 1.0, 3.0, w);
-    grid.Values.emplace_back(0.0, 0.5, 3.0, 1);
-    grid.Values.emplace_back(0.0, 0.0, 3.0, w);
-    grid.Values.emplace_back(0.5, 0.0, 3.0, 1);
+    grid.UCount = 3;
+    grid.VCount = 3;
+    grid.Values.emplace_back(0, 1000, 0, 1);
+    grid.Values.emplace_back(500, 1000, -300, 1);
+    grid.Values.emplace_back(1000, 1000, 0, 1);
+    grid.Values.emplace_back(0, 500, 0, 1);
+    grid.Values.emplace_back(500, 500, 0, 1);
+    grid.Values.emplace_back(1000, 500, 0, 1);
+    grid.Values.emplace_back(0, 0, 0, 1);
+    grid.Values.emplace_back(500, 0, 300, 1);
+    grid.Values.emplace_back(1000, 0, 0, 1);
 
     Surface surface;
     surface.DegreeU = 2;
-    surface.DegreeV = 1;
-    surface.KnotsU = KnotVector{{0.0, 0.0, 0.0, 1 / 4.0, 1 / 4.0, 1 / 2.0, 1 / 2.0, 3 / 4.0, 3 / 4.0, 1.0, 1.0, 1.0}};
-    surface.KnotsV = KnotVector{{0.0, 0.0, 1.0, 1.0}};
+    surface.DegreeV = 2;
+    surface.KnotsU = KnotVector{{0, 0, 0, 1, 1, 1}};
+    surface.KnotsV = KnotVector{{0, 0, 0, 1, 1, 1}};
     surface.ControlPoints = grid;
 
-    VecX s = VecX::LinSpaced(100, 0.0, 1.0);
-    VecX t = VecX::LinSpaced(30, 0.0, 1.0);
-
-    vector<double> px;
-    px.reserve(s.size());
-    vector<double> py;
-    py.reserve(s.size());
-    vector<double> pz;
-    pz.reserve(s.size());
-
-    for (const auto x: s)
-    {
-        auto value = surface.Evaluate(x, 0.0);
-        px.push_back(value.x());
-        py.push_back(value.y());
-        pz.push_back(value.z());
-        value = surface.Evaluate(x, 1.0);
-        px.push_back(value.x());
-        py.push_back(value.y());
-        pz.push_back(value.z());
-    }
-    plot3(px, py, pz, "r.");
-    hold(on);
-    px.clear();
-    py.clear();
-    pz.clear();
-    for (const auto y: t)
-    {
-        auto value = surface.Evaluate(0.0, y);
-        px.push_back(value.x());
-        py.push_back(value.y());
-        pz.push_back(value.z());
-        value = surface.Evaluate(1.0, y);
-        px.push_back(value.x() + 0.1);
-        py.push_back(value.y() + 0.1);
-        pz.push_back(value.z());
-    }
-    plot3(px, py, pz, "b.");
-    show();
+    return surface;
 }
 
 int main()
 {
-    //CaseA();
-    //CaseB();
-    //CaseC();
-    CaseD();
+    //ShowSurface(CaseSemiCylinder());
+    ShowSurface(CaseCylinder(), 0.1);
+    //ShowSurface(CaseSaddle());
 
     return 0;
 }
